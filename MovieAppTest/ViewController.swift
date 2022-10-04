d4a41271dd764dd79cb9e0171414c3bf611a03b4
hannatassja124 <hannatassja@gmail.com>
2022-10-04 20:00:58 +0700

    Initial Commit

---
+++ MovieAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppTest
+//
+//  Created by Hannatassja Hardjadinata on 04/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
